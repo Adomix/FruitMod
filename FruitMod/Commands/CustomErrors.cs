4c6a88f86d24a2ca0501a335843192dbc0249b67
Adomix <33987862+Adomix@users.noreply.github.com>
2018-09-20 12:05:58 -0400

    Typing up some reasons

--- FruitMod/Commands/CustomErrors.cs
+++ FruitMod/Commands/CustomErrors.cs
@@ -1,1 +1,14 @@
-
+namespace FruitMod.Commands
+{
+    public class FruitErrors
+    {
+      public enum Reasons
+      {
+        NoGuild = "You must be in a guild to use this command!",
+        WrongChannel = "You must use this command in the proper channel!",
+        Blocked = "You have been blocked from the bot!",
+        Banned = "You have been banned from the bot!",
+        Zero = "Value must be > 0!"
+      }
+    }
+}
